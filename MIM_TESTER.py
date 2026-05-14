9007be0b6d6072f0c9398c68c74c5b7024394e6d
Abhijeet Singh <abhijeet.singh_ch@external.scootsy.com>
2026-05-14 16:11:39 +0530

    Update MIM_TESTER.py

--- MIM_TESTER.py
+++ MIM_TESTER.py
@@ -65,7 +65,7 @@ THEME_VARS = DARK_VARS
 # =====================================================
 st.set_page_config(
     # page_title="Instamart — MIM Checker",
-    page_icon="SY_IICON.png",
+    page_icon="SYY_ICON.png",
     layout="wide",
     initial_sidebar_state="collapsed",
 )
